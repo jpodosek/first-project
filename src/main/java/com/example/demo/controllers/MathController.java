c523b74eeea99a6539d36239be5b3e0a3513d5f6
Jon Podosek <jpodosek@gmail.com>
2017-08-12 21:09:00 -0700

    Refactoring Pass3

--- src/main/java/com/example/demo/controllers/MathController.java
+++ src/main/java/com/example/demo/controllers/MathController.java
@@ -17,15 +17,19 @@ import com.example.demo.models.Subtractor;
 public class MathController {
 	private String pageTitle;
 	private Model model;
-	private int leftVal = 0;
-	private double rightVal = 0.00, result = 0.00;
-	private String operationTextDisplay = null;
-
+	
+	
 	public MathController() {
 		pageTitle = "Calculator";
 	}
 	
-	// what to do if user directly goes to math/calculate
+	
+	@GetMapping("calculate") // URL to which form submits; gets invoked when message is passed in browser
+	public String showIndex() {
+		return "math/index"; // ; tells Mustache to go to "adder-form"
+	}
+	/*
+	 // what to do if user directly goes to math/calculate
 	//this takes default empty values from instance variables above and plugs them into model, so mustache can render
 	@GetMapping("math/calculate") 
 	public String mathCalculatePath(Model m) {
@@ -37,6 +41,7 @@ public class MathController {
 														// result
 		return "math/index"; // ; tells Mustache to go to "adder-form"
 	}
+	*/
 
 	@PostMapping("adder")
 	// @RequestParam(name="left") is the left text box on the html page - see
@@ -69,58 +74,18 @@ public class MathController {
 	// This logic will take form input of calculate and determine what to route
 	@PostMapping("math/calculate")
 	public String performCalculation(@RequestParam(name = "leftval") int left,
-			@RequestParam(name = "rightval") double right, @RequestParam(name = "operation") String operation,
-			Model model) {
-		this.leftVal = left;
-		this.rightVal = right;
-		this.model = model;
-		Calculator calc = new Calculator(leftVal, rightVal);
-
-		if (operation.equals("add")) {
-			this.result = calc.getAddResult();
-			this.operationTextDisplay = "plus";
-
-		}
-
-		else if (operation.equals("subtract")) {
-			this.result = calc.getSubtractResult();
-			this.operationTextDisplay = "minus";
-
-		}
-
-		else if (operation.equals("multiply")) {
-			this.result = calc.getMultiplyResult();
-			this.operationTextDisplay = "mutilpied by";
-
-		}
-
-		else if (operation.equals("divide")) {
-			this.result = calc.getDivideResult();
-			this.operationTextDisplay = "divided by";
-		}
-
-		else if (operation.equals("modulo")) {
-			this.result = calc.getModuloResult();
-			this.operationTextDisplay = "mod";
-
-		}
-
-		else if (operation.equals("exponent")) {
-			this.result = calc.getExponentResult();
-			this.operationTextDisplay = "to the power of";
-		}
-
-		else {
-			System.out.println("No valid operation entered");
-			this.result = 0.00;
-			this.operationTextDisplay = "none";
-		}
-		this.model.addAttribute("leftVal", this.leftVal);
-		this.model.addAttribute("rightVal", this.rightVal);
-		this.model.addAttribute("operationText", this.operationTextDisplay);
-		this.model.addAttribute("output", this.result); // "output" is the variable name mustache uses // to display
-														// result
-
+			@RequestParam(name = "rightval") double right, @RequestParam(name = "operation") String operation
+			) 
+	{
+		
+		double result = 0.00;
+		Calculator calc = new Calculator(left, right, operation);
+		calc.Calculate();
+		
+		this.model.addAttribute("leftVal", calc.getLeftVal());		
+		this.model.addAttribute("rightVal", calc.getRightVal());
+		//this.model.addAttribute("operationText", this.operationTextDisplay);
+		this.model.addAttribute("output", result); // "output" is the variable name mustache uses to display result													
 		this.model.addAttribute("pageTitle", pageTitle);
 		return "math/calculate";
 	}
--- src/main/java/com/example/demo/models/Calculator.java
+++ src/main/java/com/example/demo/models/Calculator.java
@@ -1,20 +1,67 @@
 package com.example.demo.models;
 
 public class Calculator {
-	int first;
-	double second;
-	double add, subtract, divide, multiply, modulo, exponent;
+	public int leftVal;
+	public double rightVal;
+	//double add, subtract, divide, multiply, modulo, exponent;
+	public String operation;
 	
-	public Calculator(int first, double second) {
-			
-		add = first + second;
-		subtract = first - second;
-		multiply = first * second;
-		divide = first / second;
-		modulo = first % second;
-		exponent = Math.pow(first, second);	
+	//add = first + second;
+	//subtract = first - second;
+	//multiply = first * second;
+	//divide = first / second;
+	//modulo = first % second;
+	//exponent = Math.pow(first, second);	
+	
+	public Calculator(int first, double second, String operation) {
+			this.leftVal = first;
+			this.rightVal = second;
+			this.operation = operation;
+				
 	}
 	
+	public double Calculate() {
+		if (operation.equals("add")) {
+			return leftVal + rightVal;
+		}
+		
+		else if (operation.equals("subtract")) {
+			return leftVal - rightVal;
+		}
+		
+		else if (operation.equals("multiply")) {
+			return leftVal * rightVal;
+		}
+		
+		else if (operation.equals("divide")) {
+			return leftVal / rightVal;
+		}
+		
+		else if (operation.equals("modulo")) {
+			return leftVal % rightVal;
+		}
+		
+		else if (operation.equals("exponent")) {
+			return Math.pow(leftVal, rightVal);
+		}
+		else {
+			return 0.00;
+		}
+	}
+	
+	public int getLeftVal() {
+		return this.leftVal;
+	}
+	
+	public double getRightVal() {
+		return this.rightVal;
+	}
+	
+	public String getOp() {
+		return this.operation;
+	}
+	
+	/*
 	public double getAddResult() {
 		return add;
 	}
@@ -36,5 +83,9 @@ public class Calculator {
 	}
 	public double getExponentResult() {
 		return exponent;
-	}	
+	}
+	
+	*/
+	
+	
 }
