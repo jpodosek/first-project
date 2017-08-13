a35799ac11408dd0f2f7d9cbc546ca957653d061
Jon Podosek <jpodosek@gmail.com>
2017-08-13 12:21:34 -0700

    Refactoring Pass4: currently broken

--- src/main/java/com/example/demo/controllers/MathController.java
+++ src/main/java/com/example/demo/controllers/MathController.java
@@ -6,43 +6,56 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
 import com.example.demo.models.Adder;
 import com.example.demo.models.Calculator;
 import com.example.demo.models.Subtractor;
 
 @Controller
-@RequestMapping({ "/", "/math" }) // this allows http://localhost:8080/math/adder to work ; without, adder only
-									// works at http://localhost:8080/adder
+@RequestMapping({ "/", "/math"}) 
 public class MathController {
 	private String pageTitle;
-	private Model model;
+//	private Model model;
+	//int leftVal;
+	//double rightVal;
+	//String operation;
 	
-	
+	//Constructor
 	public MathController() {
 		pageTitle = "Calculator";
 	}
-	
+
 	
 	@GetMapping("calculate") // URL to which form submits; gets invoked when message is passed in browser
 	public String showIndex() {
-		return "math/index"; // ; tells Mustache to go to "adder-form"
+		return "math/index"; // ; reroute user to index page if they try to hit math/calculate directly
 	}
-	/*
-	 // what to do if user directly goes to math/calculate
-	//this takes default empty values from instance variables above and plugs them into model, so mustache can render
-	@GetMapping("math/calculate") 
-	public String mathCalculatePath(Model m) {
-		this.model = m;
-		this.model.addAttribute("leftVal", this.leftVal);
-		this.model.addAttribute("rightVal", this.rightVal);
-		this.model.addAttribute("operationText", this.operationTextDisplay);
-		this.model.addAttribute("output", this.result); // "output" is the variable name mustache uses // to display
-														// result
-		return "math/index"; // ; tells Mustache to go to "adder-form"
-	}
-	*/
 
+	
+	// Assignment
+	// This logic will take form input of calculate and determine what to route
+	@PostMapping("math/calculate")
+	public String performCalculation(@RequestParam(name = "leftval") int left
+									,@RequestParam(name = "rightval") double right
+									, @RequestParam(name = "operation") String operation
+									, Model model) 
+	{
+		
+		//this.leftVal = left;
+		//this.rightVal = right;
+		//this.operation = operation;
+		Calculator calc1 = new Calculator(left, right, operation); // add parameters from form's Post attributes;
+		double result = calc1.Calculate();
+		model.addAttribute("pageTitle", this.pageTitle);
+		model.addAttribute("leftVal", left);
+		model.addAttribute("rightVal", right);
+		model.addAttribute("operationText", calc1.getOp());
+		model.addAttribute("output", result); // "output" is the variable name mustache uses to display result
+		
+		return "math/calculate";
+	}
+	
+	
+	/////Old Assignment Stuff ------------------------------------------------------------------------------------------------
 	@PostMapping("adder")
 	// @RequestParam(name="left") is the left text box on the html page - see
 	// 'message-form.html'
@@ -55,6 +68,12 @@ public class MathController {
 		return "math/sum-result"; // this looks up sum-result.html
 	}
 
+	
+	@GetMapping("adder") // URL to which form submits; gets invoked when message is passed in browser
+	public String showAddingForm() {
+		
+		return "math/adder-form"; // ; tells Mustache to go to "adder-form"
+	}
 	@PostMapping("subtractor")
 	public String subTwoNumbers(@RequestParam(name = "left") int first, @RequestParam(name = "right") double second,
 			Model model) {
@@ -64,30 +83,6 @@ public class MathController {
 		model.addAttribute("sub", result);
 		return "math/difference-result"; // this looks up difference-result.html
 	}
-
-	@GetMapping("adder") // URL to which form submits; gets invoked when message is passed in browser
-	public String showAddingForm() {
-		return "math/adder-form"; // ; tells Mustache to go to "adder-form"
-	}
-
-	// Assignment
-	// This logic will take form input of calculate and determine what to route
-	@PostMapping("math/calculate")
-	public String performCalculation(@RequestParam(name = "leftval") int left,
-			@RequestParam(name = "rightval") double right, @RequestParam(name = "operation") String operation
-			) 
-	{
-		
-		double result = 0.00;
-		Calculator calc = new Calculator(left, right, operation);
-		calc.Calculate();
-		
-		this.model.addAttribute("leftVal", calc.getLeftVal());		
-		this.model.addAttribute("rightVal", calc.getRightVal());
-		//this.model.addAttribute("operationText", this.operationTextDisplay);
-		this.model.addAttribute("output", result); // "output" is the variable name mustache uses to display result													
-		this.model.addAttribute("pageTitle", pageTitle);
-		return "math/calculate";
-	}
+	
 
 }
--- src/main/java/com/example/demo/models/Calculator.java
+++ src/main/java/com/example/demo/models/Calculator.java
@@ -1,91 +1,69 @@
 package com.example.demo.models;
 
 public class Calculator {
+
 	public int leftVal;
 	public double rightVal;
-	//double add, subtract, divide, multiply, modulo, exponent;
 	public String operation;
-	
-	//add = first + second;
-	//subtract = first - second;
-	//multiply = first * second;
-	//divide = first / second;
-	//modulo = first % second;
-	//exponent = Math.pow(first, second);	
-	
-	public Calculator(int first, double second, String operation) {
-			this.leftVal = first;
-			this.rightVal = second;
-			this.operation = operation;
-				
+	public String opDisplayText;
+
+	public Calculator(int left, double right, String operation) {
+		this.leftVal = left;
+		this.rightVal = right;
+		this.operation = operation;
+		//this.leftVal = 0;
+		//this.rightVal = 0.00;
+		//this.operation = null;
+		//this.opDisplayText = null;
 	}
-	
+
 	public double Calculate() {
+		
 		if (operation.equals("add")) {
+			this.opDisplayText = "plus";
 			return leftVal + rightVal;
 		}
-		
+
 		else if (operation.equals("subtract")) {
+			this.opDisplayText = "minus";
 			return leftVal - rightVal;
 		}
-		
+
 		else if (operation.equals("multiply")) {
+			this.opDisplayText = "multiplied by";
 			return leftVal * rightVal;
 		}
-		
+
 		else if (operation.equals("divide")) {
+			this.opDisplayText = "divided by";
 			return leftVal / rightVal;
 		}
-		
+
 		else if (operation.equals("modulo")) {
+			this.opDisplayText = "mod";
 			return leftVal % rightVal;
 		}
-		
+
 		else if (operation.equals("exponent")) {
+			this.opDisplayText = "to the power of";
 			return Math.pow(leftVal, rightVal);
-		}
-		else {
+		} else {
+			this.opDisplayText = "plus";
 			return 0.00;
 		}
 	}
-	
+
 	public int getLeftVal() {
 		return this.leftVal;
 	}
-	
+
 	public double getRightVal() {
 		return this.rightVal;
 	}
-	
+
 	public String getOp() {
-		return this.operation;
+		return this.opDisplayText;
 	}
 	
-	/*
-	public double getAddResult() {
-		return add;
-	}
-	
-	public double getSubtractResult() {
-		return subtract;
-	}
-	
-	public double getMultiplyResult() {
-		return multiply;
-	}
-	
-	public double getDivideResult() {
-		return divide;
-	}
-	
-	public double getModuloResult() {
-		return modulo;
-	}
-	public double getExponentResult() {
-		return exponent;
-	}
-	
-	*/
-	
-	
+
 }
